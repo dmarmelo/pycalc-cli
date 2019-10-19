7a0721d72473650b03bd2ef1221bbbb16aff1805
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-10-19 17:51:47 +0100

    Numerical expression verification with regex.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import re
+
+
 def main():
+    regex = re.compile("^\\s*(\\d+\\s*[\\+\\-\\*\\/]\\s*)*\\d+\\s*$")
     while True:
         user_input = input("Enter your expression > ")
         if user_input == "quit":
             break
+        elif regex.match(user_input):
+            # TODO division by 0
+            print(user_input.lstrip().rstrip() + " = " + str(eval(user_input)))
         else:
-            print(user_input + " = " + str(eval(user_input)))
+            print("Invalid Expression!")
 
 
 if __name__ == '__main__':
