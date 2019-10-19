ea7947a30b82e542690dcf870b554473ccddcce0
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-10-19 18:15:59 +0100

    Exponentiation support

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import re
 
 
 def main():
-    regex = re.compile("^\\s*(\\d+(\\.\\d+)?\\s*[+\\-*/]\\s*)*\\d+(\\.\\d+)?\\s*$")
+    regex = re.compile("^\\s*(\\d+(\\.\\d+)?\\s*[+\\-*/\\^]\\s*)*\\d+(\\.\\d+)?\\s*$")
     while True:
         user_input = input("Enter your expression > ").lstrip().rstrip()
         if user_input == "quit":
             break
         elif regex.match(user_input):
             try:
-                print(user_input + " = " + str(eval(user_input)))
+                expression = user_input.replace("^", "**")
+                print(user_input + " = " + str(eval(expression)))
             except ZeroDivisionError:
                 print("ERROR Division by zero!")
 
