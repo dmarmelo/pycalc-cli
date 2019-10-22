7fee439330bbbe9d80a5e81a7fa0715c0181cb0b
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-10-22 16:08:10 +0100

    Support for constants pi and e. Regex improvements.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import regex
-from math import sqrt, sin, cos, tan, log
+from math import sqrt, sin, cos, tan, log, pi, e
 
 
 def main():
-    pattern = regex.compile(r"^\s*((?<exp>(?<dig>\d+(\.\d+)?)|((sqrt|sin|cos|tan)\s*\(\s*((?&dig)|(?&exp))\s*\))|((log)\s*\(\s*(?&dig)\s*,\s*(?&dig)\s*\)))\s*[+\-*\/\^]\s*)*(?&exp)\s*$")
+    pattern = regex.compile(r"^\s*(?<rec>((?<exp>(?<num>\d+(\.\d+)?)|((sqrt|sin|cos|tan)\s*\(\s*((?&num)|(?&rec))\s*\))|((log)\s*\(\s*((?&num)|(?&rec))\s*,\s*((?&num)|(?&rec))\s*\))|(pi|e))\s*[+\-*\/\^]\s*)*(?&exp)\s*)$")
     while True:
         raw_user_input = input("Enter your expression > ").strip()
         if raw_user_input == "quit":
