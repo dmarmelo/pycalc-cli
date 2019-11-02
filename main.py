532fe0d830149ff0242b963605f54a8ad0e61c6b
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-11-02 00:45:56 +0000

    Usage help improvements.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from math import sqrt, sin, cos, tan, log, pi, e
 
 def main():
     pattern = regex.compile(r"^\s*(?<rec>((?<exp>(?<num>\d+(\.\d+)?)|((sqrt|sin|cos|tan)\s*\(\s*((?&num)|(?&rec))\s*\))|((log)\s*\(\s*((?&num)|(?&rec))\s*,\s*((?&num)|(?&rec))\s*\))|(pi|e))\s*[+\-*\/\^]\s*)*(?&exp)\s*)$")
+    print("Type 'help' for help and 'quit' to exit.")
+    print("Enter your expression:")
     while True:
-        raw_user_input = input("Enter your expression > ").strip()
+        raw_user_input = input(">> ").strip().lower()
         if raw_user_input == "quit":
             break
+        elif raw_user_input == "help":
+            calc_help()
         elif pattern.match(raw_user_input):
             try:
                 expression = raw_user_input.replace("^", "**")
@@ -15,9 +19,25 @@ def main():
                 print(user_input + " = " + str(eval(expression)))
             except ZeroDivisionError:
                 print("ERROR Division by zero!")
-
         else:
             print("Invalid Expression!")
+
+
+def calc_help():
+    print("Available operations:")
+    print("+ -> Sum")
+    print("- -> Subtraction")
+    print("* -> Multiplication")
+    print("/ -> Division")
+    print("^ -> Exponentiation")
+    print("sqrt(n) -> Square root of n")
+    print("sin(n) -> Sine")
+    print("cos(n) -> Cosine")
+    print("tan(n) -> Tangent")
+    print("log(n, b) -> Logarithm of n to base b")
+    print("pi -> Constant Pi")
+    print("e -> Constant e")
+    print("Example: '1 + 2 * 3^4 + sqrt(3 * 2) + cos(pi) / log(10, e)'")
 
 
 if __name__ == '__main__':
